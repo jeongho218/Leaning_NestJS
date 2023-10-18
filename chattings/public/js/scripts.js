5e409fb96012293cf69f36c12146e7da0ca498f6
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-10-18 23:50:40 +0900

    socket의 emit과 on 연습 emit은 '보내는 행위', on은 '받는 행위'

--- chattings/public/js/scripts.js
+++ chattings/public/js/scripts.js
@@ -1,4 +1,4 @@
-const socket = io('/'); // socket.io의 메소드
+const socket = io('/'); // socket.io의 메소드를 사용하기 위한 준비
 const getElementById = (id) => {
   return document.getElementById(id) || null;
 };
@@ -12,6 +12,21 @@ const formElement = getElementById('chat_form');
 
 const helloUser = () => {
   const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => {
+    console.log(data);
+  });
+  // emit은 '보내는 행위'
+  // 서버에서 클라이언트로, 클라이언트에서 서버로 보낼 때도 emit
+  // prompt를 통해 입력받은 내용을 변수 username에 할당하고,
+  // 이벤트 'new_user'를 찾아간다. 여기서는 /src/chats/chats.gateway.ts이다.
+  console.log(username);
+  socket.on('hello_user', (data) => {
+    console.log(data);
+  });
+  // on은 '받는 행위
+  // 서버가 보낸 데이터를 클라이언트가 받거나,
+  // 클라이언트가 보낸 데이터를 서버가 받는 경우가 on
+  // chats.gateway.ts의 'handleNewUser'메소드에서 socket.emit을 통해 보낸 데이터를 받아 이벤트를 수행한다.
 };
 
 const init = () => {
--- chattings/src/chats/chats.gateway.ts
+++ chattings/src/chats/chats.gateway.ts
@@ -1,9 +1,25 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  @SubscribeMessage('new_user')
+  // /public/script.js의 함수 helloUser에서 socket.emit 메소드로 보낸 데이터를 받는다.
+  handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    console.log(socket.id); // 소켓의 아이디
+    // 모든 소켓은 개별적인 아이디를 가지며, 연결이 끊어질때마다 이 아이디는 사라진다. 새로 연결하면 새로운 아이디를 받는다.
+    console.log(username);
+    socket.emit('hello_user', 'Hello ' + username);
+    // 다시 emit으로 'hello_user'이벤트를 향해 보내거나
+    return 'this is return for emit!';
+    // return을 통해 값을 반환할 수도 있다.
   }
 }
