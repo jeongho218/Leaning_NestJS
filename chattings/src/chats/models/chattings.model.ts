aadf46489c21a4130137a61e4aaf1530a0e81732
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-10-21 21:32:21 +0900

    실시간 채팅을 위한 데이터 베이스 설계 채팅 내역을 저장할 chattings.model.ts와 채팅을 보낸 사람의 소켓 아이디를 저장할 sockets.model.ts를 생성함

---
+++ chattings/src/chats/models/chattings.model.ts
@@ -0,0 +1,32 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaOptions, Types } from 'mongoose';
+import { Socket as SocketModel } from './sockets.model';
+
+const options: SchemaOptions = {
+  collection: 'chattings',
+  timestamps: true,
+};
+
+@Schema(options)
+export class Chatting extends Document {
+  @Prop({
+    type: {
+      _id: { type: Types.ObjectId, required: true, ref: 'sockets' }, // sockets 모델의 _id를 공유함
+      id: { type: String }, // sockets 모델의 id
+      username: { type: String, required: true }, // sockets 모델의 username
+    },
+  })
+  @IsNotEmpty()
+  @IsString()
+  user: SocketModel;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  chat: string;
+}
+
+export const ChattingSchema = SchemaFactory.createForClass(Chatting);
--- chattings/src/main.ts
+++ chattings/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,7 +12,9 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   // views에서 렌더링하여 클라이언트에게 전달한다.
   app.setViewEngine('hbs');
-
+  // view 엔진은 hbs
+  app.useGlobalPipes(new ValidationPipe());
+  // class-validator 전역 사용
   await app.listen(8000);
 }
 bootstrap();
